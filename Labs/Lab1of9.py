7f2c0702145e019738dd0573fa2e702f05447c30
ekene enyi <42587495+CoderBlac@users.noreply.github.com>
2020-07-31 14:57:08 +0100

    Add files via upload

---
+++ Labs/Lab1of9.py
@@ -0,0 +1,14 @@
+# Browse the complete list of string methods at:
+# https://docs.python.org/3/library/stdtypes.html#string-methods 
+#and try them out here. Endeavor to copy the link without the first # comment sign beginning it.
+
+print("ekene enyi".title())
+
+full_name = "ekene enyi"
+print(full_name.islower())
+
+print(full_name.count("e"))
+
+print("ekene EnYi".captalize())
+
+print("Ekene EnYi".casefold())
---
+++ Labs/Lab2of1.py
@@ -0,0 +1,9 @@
+# Write two lines of code below, each assigning a value to a variable
+word_1 = aspiring
+word_2 = developer
+
+# Now write a print statement using .format() to print out a sentence and the 
+#   values of both of the variables
+
+print("I am a young {} software {}".format(word_1,word_2))
+
